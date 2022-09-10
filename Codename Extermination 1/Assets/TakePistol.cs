5334e4793283cbace05936496958f8ac26b5095f
Arcedii <91275440+Arcedii@users.noreply.github.com>
2022-09-10 11:32:18 +0300

    thirty commit

---
+++ Codename Extermination 1/Assets/TakePistol.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakePistol : MonoBehaviour
+{
+    public Button fire;
+    public GameObject HandsWithPistol;
+    public GameObject HandsWithoutAll;
+    public GameObject pistol;
+    void Start()
+    {
+        fire.gameObject.SetActive(false);
+        HandsWithPistol.gameObject.SetActive(false);       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fire.gameObject.SetActive(true);
+        HandsWithPistol.gameObject.SetActive(true);       
+        HandsWithoutAll.gameObject.SetActive(false);
+        Destroy(pistol);
+    }
+    
+}
